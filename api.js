d38856b609d92f993b1ceec063efa433460fa215
KOSALAI V <kosalaiv@KOSALAIs-MacBook-Pro.local>
2022-11-27 18:58:59 +0530

    app change 1

--- api.js
+++ api.js
@@ -17,38 +17,38 @@ app.get('/board',(req,res)=>{
   res.send({ description: "Its working!!"})
 })
 
-io.on("connection", (socket) => {
-  console.log("joined",socket.id)
-  socket.on("join-room", (room) => {
-    socket.join(room);
-  });
-  socket.on("make-button-enabled", (value, room) => {
-    io.to(room).emit("receive-make-button-enabled", value);
-  });
-  socket.on("make-blur", (value, room) => {
-    io.to(room).emit("receive-make-blur", value);
-  });
-  socket.on("went-well", (value, room) => {
-    io.to(room).emit("receive-went-well", value);
-  });
-  socket.on("to-improve", (value, room) => {
-    io.to(room).emit("receive-to-improve", value);
-    //socket.broadcast.emit('receive-to-improve',value)
-  });
-  socket.on("action-items", (value, room) => {
-    // persistdata.data3=value;
-    io.to(room).emit("receive-action-items", value);
-  });
-  socket.on("went-well-comment", (value, room) => {
-    io.to(room).emit("receive-went-well-comment", value);
-  });
-  socket.on("to-improve-comment", (value, room) => {
-    io.to(room).emit("receive-to-improve-comment", value);
-  });
-  socket.on("action-item-comment", (value, room) => {
-    io.to(room).emit("receive-action-item-comment", value);
-  });
-});
+// io.on("connection", (socket) => {
+//   console.log("joined",socket.id)
+//   socket.on("join-room", (room) => {
+//     socket.join(room);
+//   });
+//   socket.on("make-button-enabled", (value, room) => {
+//     io.to(room).emit("receive-make-button-enabled", value);
+//   });
+//   socket.on("make-blur", (value, room) => {
+//     io.to(room).emit("receive-make-blur", value);
+//   });
+//   socket.on("went-well", (value, room) => {
+//     io.to(room).emit("receive-went-well", value);
+//   });
+//   socket.on("to-improve", (value, room) => {
+//     io.to(room).emit("receive-to-improve", value);
+//     //socket.broadcast.emit('receive-to-improve',value)
+//   });
+//   socket.on("action-items", (value, room) => {
+//     // persistdata.data3=value;
+//     io.to(room).emit("receive-action-items", value);
+//   });
+//   socket.on("went-well-comment", (value, room) => {
+//     io.to(room).emit("receive-went-well-comment", value);
+//   });
+//   socket.on("to-improve-comment", (value, room) => {
+//     io.to(room).emit("receive-to-improve-comment", value);
+//   });
+//   socket.on("action-item-comment", (value, room) => {
+//     io.to(room).emit("receive-action-item-comment", value);
+//   });
+// });
 
 app.listen(process.env.PORT || 3000,()=>{
   console.log("port is listening to 3000")
